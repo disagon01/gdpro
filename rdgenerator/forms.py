d7b34703b994df4f80ec305ecc0fbb1fa032f2af
disagon01 <100069014+disagon01@users.noreply.github.com>
2026-01-14 23:02:17 +0800

    Update forms.py

--- rdgenerator/forms.py
+++ rdgenerator/forms.py
@@ -82,6 +82,9 @@ class GenerateForm(forms.Form):
     cycleMonitor = forms.BooleanField(initial=False, required=False)
     xOffline = forms.BooleanField(initial=False, required=False)
     removeNewVersionNotif = forms.BooleanField(initial=False, required=False)
+    hidePassword = forms.BooleanField(initial=False,required=False,label="移除密码显示(仅允许传入连接模式下勾选)")
+    hideMenuBar = forms.BooleanField(initial=False,required=False,label="移除三点菜单(仅允许传入连接模式下勾选)")
+    removeTopNotice = forms.BooleanField(initial=True,required=False,label="移除顶部温馨提示（固定密码默认会显示）")
 
     def clean_iconfile(self):
         print("checking icon")
