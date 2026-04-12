f90db51f254e655fb42c41c94abab06b999acb7a
disagon01 <100069014+disagon01@users.noreply.github.com>
2026-04-12 16:50:55 +0800

    Update forms.py

--- rdgenerator/forms.py
+++ rdgenerator/forms.py
@@ -18,7 +18,6 @@ class GenerateForm(forms.Form):
     version = forms.ChoiceField(
         choices=[
             ('master', '开发版（每夜构建）'),
-            ('1.4.6', '1.4.6'),
             ('1.4.5', '1.4.5'),
             ('1.4.4', '1.4.4'),
             ('1.4.3', '1.4.3'),
@@ -33,7 +32,7 @@ class GenerateForm(forms.Form):
             ('1.3.4', '1.3.4'),
             ('1.3.3', '1.3.3')
         ],
-        initial='1.4.6',
+        initial='1.4.5',
         label="版本"
     )
     help_text = "“开发版”是包含最新功能的每夜构建版本，但可能稳定性较低"
